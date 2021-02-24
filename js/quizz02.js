daba02b987456c13018df99c88abee0abe0dfa7a
InmaG <inmags20@outlook.es>
2021-02-24 16:04:34 +0100

    Modify css, js y html and delete img

--- js/quizz02.js
+++ js/quizz02.js
@@ -73,7 +73,7 @@ let questions = [
     },
     {
         title: "¿Cuantos huesos tiene el ser humano?",
-        correctAnswer: 2,
+        correctAnswer: 3,
         answers: [
             {
                 label: "200 huesos",
@@ -188,7 +188,7 @@ let questions = [
     1. PrintTitle; se le pasa como argumento el objeto de preguntas
     2. PrintAllAnswers
 */
-
+let counter = 0;
 //Funcion para crear elementos html
 
 function createElement(el) {
@@ -199,12 +199,12 @@ function createElement(el) {
 // Crea la pregunta, consistente en un nodo p y textNode, y es hijo de un div
 // Input: question[i].title
 
-function printTitle(title) {
+function printTitle(title, container) {
     let questionDiv, questionTitle, questionText;
     // Creamos el nodo div y lo hacemos hijo de form
     questionDiv = createElement('div');
     questionDiv.setAttribute("class", "pregunta");
-    form.appendChild(questionDiv);
+    container.appendChild(questionDiv);
     // Creamos el nodo p y lo hacemos hijo de div
     questionTitle = createElement('p');
     questionDiv.appendChild(questionTitle);
@@ -216,19 +216,21 @@ function printTitle(title) {
 
 // Crea las respuestas, consistentes en inputs radio, con su label y son hijos de un div
 
-function printAllAnswers(questions) {
+function printAllAnswers(answers, correctAnswer, container) {
     // Creamos tantos divs como respuestas haya, añadimos el atributo clase y los hacemos hijo de form
     // Creamos los arrays de labels, inputs y answers e invocamos a las funciones que crean los nodos label e input
     let arrLabels = [], arrInputs = [], arrAnswers = [], divAnswers;
-    for (let i = 0; i < questions.length; i++) {
+    for (let i = 0; i < answers.length; i++) {
         divAnswers = document.createElement("div");
         divAnswers.setAttribute("class", "respuestas");
-        form.appendChild(divAnswers);
-        arrAnswers.push(questions[i])
-        arrLabels[arrLabels.length] = printLabel(questions[i]);
-        arrInputs[arrInputs.length] = printInput(questions[i]);
+        container.appendChild(divAnswers);
+        arrAnswers.push(answers[i])
+        arrLabels[arrLabels.length] = printLabel(answers[i], i, correctAnswer, container) ;
+        arrInputs[arrInputs.length] = printInput(answers[i]);
         divAnswers.appendChild(arrLabels[i])
         divAnswers.appendChild(arrInputs[i])
+
+        
     };
 
 };
@@ -236,7 +238,7 @@ function printAllAnswers(questions) {
 // Crea nodo label y su textNode y appenChild del textNode al label
 // Input: subarray de answers
 // Return label
-function printLabel(answers) {
+function printLabel(answers,position,correctAnswer,container) {
 
     // Creamos el nodo label
     let label = createElement('label');
@@ -247,6 +249,7 @@ function printLabel(answers) {
     let labelText = document.createTextNode(answers.label);
     // Convertimos a textNode en hijo de label
     label.appendChild(labelText);
+    label.addEventListener("click", () => validadora(position, correctAnswer,label, container));
     // Devolvemos el nodo
     return label;
 };
@@ -264,6 +267,12 @@ function printInput(answers) {
     input.setAttribute('type', 'radio');
     input.setAttribute('id', answers.id);
     input.setAttribute('id', answers.value);
+
+    input.classList.add('circulosinputs');
+    console.log(input);
+    if(input.checked = true){
+        console.log('checked')
+    }
     // Devolvemos el nodo
     return input;
 
@@ -271,7 +280,64 @@ function printInput(answers) {
 const body = document.querySelector("body");
 let form = createElement("form");
 body.appendChild(form);
-for (let i = 0; i < questions.length; i++) {
-    printTitle(questions[i].title);
-    printAllAnswers(questions[i].answers);
+
+//funcion pintar solo una pregunta
+
+function printQuestion(question){
+    let container = document.createElement("div");
+    form.appendChild(container);
+    printTitle(question.title, container);
+    printAllAnswers(question.answers, question.correctAnswer, container);
+
 }
+
+printQuestion(questions[counter]);
+
+//botón enviar al hacer click dispara función validadora 
+//función validadora: 
+
+
+function validadora (position, correctAnswer, label, container){
+    console.log(position, correctAnswer);
+   
+    if(correctAnswer === position){
+        label.classList.add("labelcorrect");
+        setTimeout(function(){
+            ereaseQuestion(container);
+            counter++;
+            if(counter<questions.length){
+                printQuestion(questions[counter]);
+            } else{
+
+                let paginaFinal = document.createElement("p");
+                form.appendChild(paginaFinal);
+                paginaFinal = 'Hemos terminado, gracias por jugar con nosostros';
+                
+            }
+        },800);
+        
+    }else {
+        label.classList.add("labelerror");
+
+    }
+
+    
+}
+
+// function createFinal(){
+//     let paginaFinal = document.createElement("p");
+//     form.appendChild(paginaFinal);
+
+// }
+function ereaseQuestion(container){
+    container.remove();
+}
+
+let inputc = document.getElementsByTagName ("input");
+
+
+inputc[0].addEventListener("click", () =>{
+    console.log("click", inputc[0].checked);
+    console.log("ansers checked", questions[0].answers[1]);
+    console.log ("correcta", questions[0].correctAnswer);
+})
